88a2902db85f5ba17afd770f385c8d4a9ef75290
gianv322 <90941304+gianv322@users.noreply.github.com>
2022-02-22 23:08:39 -0500

    commit tree c55d45bfcecf88b5b0b617ca0dd6dfe33f5f46be

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!" 
+end
+
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+return 2 + 5
+
+def halve(number = 2)
+    "#{number}/2"
+end
+
